4eb3fd3721821368ca6444031e5dc70fd1346e5f
Marco Ferreira <marcoavfcc@gmail.com>
2021-05-06 11:11:19 -0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../service/stripe";
 
 import styles from './home.module.scss'
 
@@ -25,4 +27,10 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1InvIuIlhDUGPsULTwyG0iY9', {
+    expand: ['product']
+  })
 }

b730caf2e348b5320f0c38995f29d7ce56284899
Marco Ferreira <marcoavfcc@gmail.com>
2021-05-08 16:26:46 -0300

    atualizado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../service/fauna';
 
 export default NextAuth({
     providers: [
@@ -9,4 +13,23 @@ export default NextAuth({
             scope: 'read:user'
         }),
     ],
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user
+
+            try{
+                await fauna.query(
+                    q.Create(
+                        q.Collection('users'),
+                        { data: { email } }
+                    )
+                )
+                return true;
+            } catch {
+                return false;
+            }
+
+            
+        }
+    }
 })
